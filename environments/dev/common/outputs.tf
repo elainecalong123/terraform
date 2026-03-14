7101d150c37003b3646a140dad72574b29adcde0
elainecalong123 <elainecalong123@gmail.com>
2026-03-14 20:17:01 +0800

    check github actions

--- environments/dev/common/outputs.tf
+++ environments/dev/common/outputs.tf
@@ -1,21 +1,18 @@
+output "public_zone_id" {
+  value       = aws_route53_zone.main.zone_id
+  description = "The ID of the public Route 53 zone for cambridgelaine.com"
+}
+
+### PRIMARY VPC
+
 output "primary_vpc_id" {
   description = "The ID of the VPC in Singapore"
   value       = module.vpc_primary.vpc_id
 }
 
-output "dr_vpc_id" {
-  description = "The ID of the VPC in Sydney"
-  value       = module.vpc_dr.vpc_id
-}
-
 output "primary_private_route_table_id" {
   description = "The private route table for Singapore"
   value       = module.vpc_primary.private_route_table_id
-}
-
-output "dr_private_route_table_id" {
-  description = "The private route table for Sydney"
-  value       = module.vpc_dr.private_route_table_id
 }
 
 output "primary_public_subnets" {
@@ -28,24 +25,31 @@ output "primary_private_subnets" {
   value       = module.vpc_primary.private_subnet_ids
 }
 
-output "public_zone_id" {
-  value       = aws_route53_zone.main.zone_id
-  description = "The ID of the public Route 53 zone for cambridgelaine.com"
-}
-
 output "primary_database_subnet_ids" {
   description = "Database subnets in Singapore"
   value       = module.vpc_primary.database_subnet_ids
 }
 
-output "dr_database_subnet_ids" {
-  description = "Database subnets in Sydney"
-  value       = module.vpc_dr.database_subnet_ids
-}
-
 output "primary_vpc_cidr" {
   description = "The CIDR block of the Singapore VPC"
   value       = module.vpc_primary.vpc_cidr_block
+}
+
+
+### DISASTER RECOVERY VPC
+output "dr_vpc_id" {
+  description = "The ID of the VPC in Sydney"
+  value       = module.vpc_dr.vpc_id
+}
+
+output "dr_private_route_table_id" {
+  description = "The private route table for Sydney"
+  value       = module.vpc_dr.private_route_table_id
+}
+
+output "dr_database_subnet_ids" {
+  description = "Database subnets in Sydney"
+  value       = module.vpc_dr.database_subnet_ids
 }
 
 output "dr_public_subnets" {
